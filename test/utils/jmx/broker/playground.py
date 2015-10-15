f785d16b14814e70454bf1bde08dc55586ad7abd
Tim Preece <tdpreece@gmail.com>
2015-10-15 19:39:10 +0100

    Remove get raw requests that have been implemented in RemoteJMXQueue.

--- test/utils/jmx/broker/playground.py
+++ test/utils/jmx/broker/playground.py
@@ -14,16 +14,6 @@ get_queue_size_payload = {
 session.request(get_queue_size_payload)
 
 
-def get_queue_size():
-    session = JolokiaSession.connect('localhost', '28161')
-    get_queue_size_payload = {
-        "type": "read",
-        "mbean": "org.apache.activemq:type=Broker,brokerName=TEST.BROKER,destinationType=Queue,destinationName=test.req",
-        "attribute": "QueueSize"
-    }
-    queue_size = session.request(get_queue_size_payload)
-    print "Queue size = {}".format(queue_size)
-
 url  = 'http://localhost:28161/api/jolokia'
 create_queue_payload = {
     "type":"exec",
@@ -48,7 +38,6 @@ r = requests.post(url, json=send_text_message_payload)
 print "status code is 200 is {}".format(r.status_code == 200)
 print r
 
-print get_queue_size()
 print "Queue size = {}".format(queue.get_size())
 
 browse_messages_payload = {
@@ -67,4 +56,4 @@ purge_queue_payload = {
 r = requests.post(url, json=purge_queue_payload)
 print r.text
 
-print get_queue_size()
+print "Queue size = {}".format(queue.get_size())
