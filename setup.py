a04fcc2a4716429334c2cfde97d8b35b9308f0f3
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2015-10-28 18:14:11 +0000

    Prepare the deployment scripts (v0.0.3)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-VERSION = '0.0.1'
+VERSION = '0.0.3'
 
 setup(
     name = 'tdl-client-python',
@@ -8,10 +8,10 @@ setup(
     package_dir = {'': 'src'},
     version = VERSION,
     description = 'tdl-client-python',
-    author = 'Tim Preece',
-    author_email = 'tdpreece@gmail.com',
+    author = 'Tim Preece, Julian Ghionoiu',
+    author_email = 'tdpreece@gmail.com, julian.ghionoiu@gmail.com',
     url = 'https://github.com/julianghionoiu/tdl-client-python',
-    download_url = 'https://github.com/peterldowns/tdl-client-python/tarball/' + VERSION,
+    download_url = 'https://github.com/julianghionoiu/tdl-client-python/archive/v{0}.tar.gz'.format(VERSION),
     keywords = ['kata', 'activemq', 'rpc'],
     classifiers = [],
 )
