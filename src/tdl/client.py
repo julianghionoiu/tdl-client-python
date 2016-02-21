a4d774d56b63777235060d28eec69d905ba48047
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2016-02-21 12:07:29 +0000

    Version v0.9

--- src/tdl/client.py
+++ src/tdl/client.py
@@ -87,7 +87,7 @@ class ApplyProcessingRules(object):
             return str(parameter)
         lines = str(parameter).split('\n')
         if len(lines) == 1:
-            return lines[0]
+            return '"{}"'.format(lines[0])
         if len(lines) == 2:
             return '"{} .. ( 1 more line )"'.format(lines[0])
         return '"{} .. ( {} more lines )"'.format(lines[0], len(lines) - 1)
