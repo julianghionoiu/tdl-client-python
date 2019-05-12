93a2e4d59e2295c4af73012dfbb75975e4447da3
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-05-12 13:49:40 +0100

    Unifiy the start/stop scripts

--- stopExternalDependencies.sh
+++ stopExternalDependencies.sh
@@ -4,29 +4,16 @@ set -e
 set -u
 set -o pipefail
 
-stopProcessAtPort() {
-    PORT=$1
-    PID=$(netstat -tulpn | grep :${PORT} | awk '{print $7}' | tr -d "/java" || true)
-    if [[ -z "${PID}" ]]; then
-        echo "~~~~~~~~~~ Process on port ${PORT} stopped ~~~~~~~~~"
-    else
-        kill -9 ${PID}
-        echo "~~~~~~~~~~ Process on port ${PORT} killed ~~~~~~~~~"
-    fi
+startWiremocks() {
+    echo "~~~~~~~~~~ Starting Wiremocks on ports 41375 and 8222 ~~~~~~~~~"
+    python3 wiremock/wiremock-wrapper.py stop 41375
+    python3 wiremock/wiremock-wrapper.py stop 8222
 }
 
-stopWiremocks() {
-    echo "~~~~~~~~~~ Stopping Wiremocks listening on ports 41375 and 8222 ~~~~~~~~~"
-    python3 wiremock/wiremock-wrapper.py stop || true
-
-    stopProcessAtPort 41375
-    stopProcessAtPort 8222
+startBroker() {
+    echo "~~~~~~~~~~ Starting Broker ~~~~~~~~~"
+    python3 broker/activemq-wrapper.py stop
 }
 
-stopBroker() {
-    echo "~~~~~~~~~~ Stoping Broker ~~~~~~~~~"
-    python3 broker/activemq-wrapper.py stop || true
-}
-
-stopWiremocks
-stopBroker
+startWiremocks
+startBroker
