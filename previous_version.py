5209b34e6cb6efae3d5788aa3c9f84b804ae9da3
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2022-07-17 14:43:57 +0100

    Update setup.py

--- previous_version.py
+++ previous_version.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-PREVIOUS_VERSION = '0.27.1'
+PREVIOUS_VERSION = '0.27.2'
 
 
 def main():
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 # !!!!!!! MAJOR DEBT - This is hardcoded
-VERSION = "0.27.1"
+VERSION = "0.27.2"
 
 # ~~~~~ Create configuration
 
