0486259273e1772ed663223e35cc6ece53fa5b01
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2024-06-28 15:10:45 +0100

    Attempting to fix the dist

--- previous_version.py
+++ previous_version.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-PREVIOUS_VERSION = '0.27.10'
+PREVIOUS_VERSION = '0.27.11'
 
 
 def main():
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 # !!!!!!! MAJOR DEBT - This is hardcoded
-VERSION = "0.27.11"
+VERSION = "0.27.12"
 
 # ~~~~~ Create configuration
 from pathlib import Path
