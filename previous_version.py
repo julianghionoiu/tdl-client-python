42e936f5b43c1d59080f57b3cd9f9fd5b8880417
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2024-06-28 15:26:02 +0100

    Bump version

--- previous_version.py
+++ previous_version.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-PREVIOUS_VERSION = '0.27.12'
+PREVIOUS_VERSION = '0.27.13'
 
 
 def main():
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 # !!!!!!! MAJOR DEBT - This is hardcoded
-VERSION = "0.27.13"
+VERSION = "0.27.14"
 
 # ~~~~~ Create configuration
 from pathlib import Path
